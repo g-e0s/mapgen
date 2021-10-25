1079f0da50717f005a399718c5c716685402d7a4
Egor Spirin <spirin.egor@mail.ru>
2021-10-25 20:40:34 +0300

    Prettify code with black formatter

--- run.py
+++ run.py
@@ -2,7 +2,13 @@ from mapgen import Dungeon, VideoRecorder
 
 
 if __name__ == "__main__":
-    env = VideoRecorder(Dungeon(30, 30, min_room_xy=7, max_room_xy=14, max_steps=200), video_path="videos", size=512, fps=60, extension="gif")    
+    env = VideoRecorder(
+        Dungeon(30, 30, min_room_xy=7, max_room_xy=14, max_steps=200),
+        video_path="videos",
+        size=512,
+        fps=60,
+        extension="gif",
+    )
     env.seed(10)
 
     state, done = env.reset(), False
